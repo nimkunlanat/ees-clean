7c8f701e007aa83f56cc7e86b245e7346eedebb7
khaiwhan <sritonchai.p@gmail.com>
2024-02-05 01:42:32 +0700

    [ component ] : add animation rerender

--- Web/ClientApp/src/app/feature/demo/panel/panel.component.ts
+++ Web/ClientApp/src/app/feature/demo/panel/panel.component.ts
@@ -10,7 +10,7 @@ export class PanelComponent {
   items: SelectItem[] = [];
 
   constructor() {
-    for (let index = 0; index < 3; index++) this.items.push({value : index + 1, label: `Header ${index + 1}` })
+    for (let index = 0; index < 3; index++) this.items.push({ value: index + 1, label: `Header ${index + 1}` })
   }
 
 }
