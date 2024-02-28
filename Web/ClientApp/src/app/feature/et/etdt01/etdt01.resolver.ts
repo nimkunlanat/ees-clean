1fb83b54fc3596fbfda9e814f9505b600fe5dd01
chaowarin.a <chaowarin.a@rmutsvmail.com>
2024-02-28 17:15:16 +0700

    [etedt01]:Skillmatrix edit front-end

--- Web/ClientApp/src/app/feature/et/etdt01/etdt01.resolver.ts
+++ Web/ClientApp/src/app/feature/et/etdt01/etdt01.resolver.ts
@@ -1,13 +1,19 @@
 import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, ResolveFn, Router, RouterStateSnapshot } from '@angular/router';
+import { forkJoin, map, of } from 'rxjs';
 import { Etdt01Service } from './etdt01.service';
 
 export const etdt01: ResolveFn<boolean> = (route, state) => {
   return true;
 };
 
-export const Assessment: ResolveFn<any[]> = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => inject(Etdt01Service).list()
+export const Assessment: ResolveFn<any> = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  let params:any = inject(Router).getCurrentNavigation()?.extras.state;
+  let listAssessment = inject(Etdt01Service).listAssessment()
+  return forkJoin(listAssessment).pipe(map((res) => {
+    let listAssessment = res[0];
+    return {listAssessment , params}
+  }))
+}
 
-export const etdt01SkillResolver: ResolveFn<boolean> = (route, state) => {
-  return true;
-};
+export const Skillmatrix: ResolveFn<any[]> = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => inject(Etdt01Service).listSkillmatrix()
